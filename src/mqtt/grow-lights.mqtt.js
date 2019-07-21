ef9f2ff8f95b55621d2419809abf677dc95a1e9c
Ruy Garcia <rrgarciach@gmail.com>
2019-07-21 03:56:45 -0500

    Adding cron

--- src/mqtt/grow-lights.mqtt.js
+++ src/mqtt/grow-lights.mqtt.js
@@ -61,9 +61,9 @@ async function turnOff() {
 }
 
 const CronJob = require('cron').CronJob;
-new CronJob('50 3 * * * *', async function () {
+new CronJob('* 57 3 * * *', async function () {
   await turnOn();
 });
-new CronJob('* 0 * * * *', async function () {
+new CronJob('* * 0 * * *', async function () {
   await turnOff();
 });
