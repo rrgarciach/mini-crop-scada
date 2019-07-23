5cc2474f5b143a4d088cf9333fdd674dff410e8a
Ruy Garcia <rrgarciach@gmail.com>
2019-07-23 02:12:52 -0500

    Using fs exists

--- src/lib/json-store/index.js
+++ src/lib/json-store/index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
 function save(data, fileName) {
-  if (!fs.existsSync(fileName)) fs.openSync(fileName, 'w');
+  if (!fs.existsSync(fileName)) fs.writeFileSync(fileName, {}, {flags: 'w'});
   const strData = JSON.stringify(data);
   fs.writeFileSync(fileName, strData);
 }
 
 function load(fileName) {
-  if (!fs.existsSync(fileName)) fs.openSync(fileName, 'w');
+  if (!fs.existsSync(fileName)) fs.writeFileSync(fileName, {}, {flags: 'w'});
   const rawData = fs.readFileSync(fileName);
   return JSON.parse(rawData);
 }
