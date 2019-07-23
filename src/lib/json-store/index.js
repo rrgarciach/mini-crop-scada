31325dbf3e3948f06c10bc395f14af6d5dfdffb6
Ruy Garcia <rrgarciach@gmail.com>
2019-07-23 01:44:04 -0500

    Added flag on reading JSON

--- src/lib/json-store/index.js
+++ src/lib/json-store/index.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 
 function save(data, fileName) {
   const strData = JSON.stringify(data);
-  fs.writeFileSync(fileName, strData);
+  fs.writeFileSync(fileName, strData, {flag: 'w'});
 }
 
 function load(fileName) {
-  const rawData = fs.readFileSync(fileName);
+  const rawData = fs.readFileSync(fileName, {flag: 'w'});
   return JSON.parse(rawData);
 }
 
